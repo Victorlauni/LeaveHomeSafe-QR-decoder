3e0a3516be039b4e370cbb07ed7344d178088520
Victor Lau <vtleluc@gmail.com>
2021-02-13 21:20:43 +0800

    Add HKEN validation

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const decode = function(msg) {
-  if (typeof msg === 'string') {
+  if (typeof msg === 'string' && msg.substr(0,5) === "HKEN:") {
     const HKEN = msg.substr(0,6)
     const LocateID = msg.substr(6, 8)
-    const hash = msg.substr(14)
-    const decodedMsg = JSON.parse(Buffer.from(hash, 'base64').toString())
-    return decodedMsg
+    try {
+      const hash = msg.substr(14)
+      const decodedMsg = JSON.parse(Buffer.from(hash, 'base64').toString())
+      return decodedMsg
+    } catch (err) {
+      console.error("Invalid QR code")
+      return null
+    }
+    
   }
   else return null
 }
